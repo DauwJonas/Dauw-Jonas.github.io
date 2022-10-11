ef9ebd46d474ed9f6bbd76a86431b1eeebc4383d
Dauw_Jonas <43506590+DauwJonas@users.noreply.github.com>
2022-10-11 22:08:03 +0200

    Remove _Back-Ups

--- _Back-Ups/Back-Up (2020-02-13 15-20) Factorio Fan Website/js/Reviews-Videos.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("NSFWReviewsButtonShow").addEventListener("click", function() {
-    var El = document.getElementById("NSFWReviews");
-    El.parentElement.removeChild(El);
-    var El = new Array();
-    El = document.getElementsByClassName("NSFW");
-    for (let i = 0; i < El.length; i++) {
-        El[i].style.display = "block";
-    }
-});
